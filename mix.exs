1da1d73f0f4e38893dfc75937f3d16fb44bd1ef8
Unnawut Leepaisalsuwanna <unnawut@unnawut.com>
2017-12-05 23:42:35 +0700

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Licensir.Mixfile do
   def project do
     [
       app: :licensir,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.5",
       escript: [main_module: Licensir.Licenses],
       deps: deps(),
