d7490e71f1315fc9bd816377c629fde4c972d703
Unnawut Leepaisalsuwanna <unnawut@omise.co>
2018-03-31 11:40:34 +0700

    Version v0.2.7

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Licensir.Mixfile do
   def project do
     [
       app: :licensir,
-      version: "0.2.6",
+      version: "0.2.7",
       elixir: "~> 1.5",
       escript: [main_module: Licensir.Licenses],
       deps: deps(),
