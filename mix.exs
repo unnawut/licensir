9a14bc001bf7acb1e86ad646e467cbf11b09d12e
Unnawut Leepaisalsuwanna <unnawut@unnawut.com>
2019-05-30 12:09:15 +0700

    Release 0.4.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Licensir.Mixfile do
   def project do
     [
       app: :licensir,
-      version: "0.4.0",
+      version: "0.4.1",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       escript: [main_module: Licensir.Licenses],
