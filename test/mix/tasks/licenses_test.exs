58b5273d15016ea3c8bb93e30ce0836b4cfbfa2d
Unnawut Leepaisalsuwanna <unnawut@unnawut.com>
2019-08-26 01:17:52 +0700

    Fix unit test

--- test/mix/tasks/licenses_test.exs
+++ test/mix/tasks/licenses_test.exs
@@ -9,19 +9,22 @@ defmodule Licensir.Mix.Tasks.LicensesTest do
       end)
 
     expected =
-      IO.ANSI.format([
+      IO.ANSI.format_fragment([
         [:yellow, "Notice: This is not a legal advice. Use the information below at your own risk."], :reset, "\n",
-        "dep_license_undefined   -> ", [:red, "Undefined"], :reset, "\n",
-        "dep_of_dep              -> ", [:red, "Undefined"], :reset, "\n",
-        "dep_one_license         -> ", [:green, "Licensir Mock License"], :reset, "\n",
-        "dep_one_unrecognized_license_file -> ", [:red, "Unrecognized license file content"], :reset, "\n",
-        "dep_two_conflicting_licenses -> ", [:yellow, "Unsure (found: License One, Licensir Mock License)"], :reset, "\n",
-        "dep_two_licenses        -> ", [:green, "License Two, License Three"], :reset, "\n",
-        "dep_two_variants_same_license -> ", [:green, "Apache 2.0"], :reset, "\n",
-        "dep_with_dep            -> ", [:red, "Undefined"], :reset, "\n"
+        "+-----------------------------------+---------+----------------------------------------------------+", "\n",
+        "| Package                           | Version | License                                            |", "\n",
+        "+-----------------------------------+---------+----------------------------------------------------+", "\n",
+        "| dep_license_undefined             |         | Undefined                                          |", "\n",
+        "| dep_of_dep                        |         | Undefined                                          |", "\n",
+        "| dep_one_license                   |         | Licensir Mock License                              |", "\n",
+        "| dep_one_unrecognized_license_file |         | Unrecognized license file content                  |", "\n",
+        "| dep_two_conflicting_licenses      |         | Unsure (found: License One, Licensir Mock License) |", "\n",
+        "| dep_two_licenses                  |         | License Two, License Three                         |", "\n",
+        "| dep_two_variants_same_license     |         | Apache 2.0                                         |", "\n",
+        "| dep_with_dep                      |         | Undefined                                          |", "\n",
+        "+-----------------------------------+---------+----------------------------------------------------+", "\n", "\n"
       ])
       |> to_string()
-      |> Kernel.<>("\n")
 
     assert output == expected
   end
