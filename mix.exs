2eb0c21939217d96f0c59d820cf52806f5fc8cac
Kian Meng Ang <kianmeng.ang@gmail.com>
2021-06-06 12:57:41 +0800

    Misc doc changes (#22)
    Besides other documentation changes, this commit ensures the generated
    HTML doc for HexDocs.pm will become the source of truth for this Elixir
    library and leverage on latest features of ExDoc.

--- mix.exs
+++ mix.exs
@@ -1,18 +1,20 @@
 defmodule Licensir.Mixfile do
   use Mix.Project
 
+  @source_url "https://github.com/unnawut/licensir"
+  @version "0.6.2"
+
   def project do
     [
       app: :licensir,
-      version: "0.6.2",
+      version: @version,
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       escript: [main_module: Licensir.Licenses],
-      deps: deps(),
       name: "Licensir",
-      description:
-        "An Elixir mix task that list the license(s) of all installed packages in your project.",
+      deps: deps(),
       package: package(),
+      docs: docs(),
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
         coveralls: :test,
@@ -29,17 +31,34 @@ defmodule Licensir.Mixfile do
 
   defp package do
     [
+      description:
+        "An Elixir mix task that list the license(s) " <>
+          "of all installed packages in your project.",
       files: ["lib", "priv", "mix.exs", "README.md", "LICENSE"],
       maintainers: ["Unnawut Leepaisalsuwanna"],
       licenses: ["MIT"],
-      links: %{"GitHub" => "https://github.com/unnawut/licensir"}
+      links: %{"GitHub" => @source_url}
     ]
   end
 
   defp deps do
     [
-      {:ex_doc, ">= 0.19.0", only: :dev},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:excoveralls, "~> 0.8", only: :test}
+    ]
+  end
+
+  defp docs do
+    [
+      extras: [
+        "LICENSE.md": [title: "License"],
+        "README.md": [title: "Overview"]
+      ],
+      main: "readme",
+      source_url: @source_url,
+      source_ref: "v#{@version}",
+      formatters: ["html"],
+      filter_prefix: "Licensir"
     ]
   end
 end
